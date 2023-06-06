f913c74860f1faa3bb10c44d21f10048e73b662e
Diaphanous (TGB) <65540686+techygamebar@users.noreply.github.com>
2023-06-06 17:49:56 +0530

    reworked server.lua

--- server.lua
+++ server.lua
@@ -3,5 +3,23 @@ TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
 
 RegisterServerEvent('qb-watercooler:refillThirst')
 AddEventHandler('qb-watercooler:refillThirst', function()
-	TriggerServerEvent("QBCore:Server:SetMetaData", "thirst", QBCore.Functions.GetPlayerData().metadata["thirst"] + Consumeables[itemName])
+	local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+
+oldthirst= Player.PlayerData.metadata['thirst']
+print(oldthirst)
+
+if oldthirst<100  then
+	
+newThirst=oldthirst+10
+print(newThirst)
+TriggerClientEvent("anim",source,true)
+Player.Functions.SetMetaData('thirst', newThirst)
+TriggerClientEvent('hud:client:UpdateNeeds', src, Player.PlayerData.metadata['hunger'], newThirst)
+	    Player.Functions.Save()
+elseif  oldthirst>100 or oldthirst==100 then
+	print("full")
+	TriggerClientEvent("anim",source,false)
+
+end
 end)
